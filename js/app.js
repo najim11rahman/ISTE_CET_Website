ea55c41f8344cafa4eee8cdfb3647a6902a61515
taqnar <muzammilthottathil.official@gmail.com>
2020-08-06 07:00:16 +0530

    added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log("Welcome");
+
+const hamburger = () => {
+  var hamburgerDiv = document.querySelector("#hamburger");
+  var navList = document.querySelector("#navList");
+
+  console.log("clicked");
+  hamburgerDiv.classList.toggle("is-active");
+  navList.classList.toggle("show-list");
+};
