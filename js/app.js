764c1c128f006a82bfb81687387ebf80b35b8cc6
taqnar <muzammilthottathil.official@gmail.com>
2020-08-06 15:17:36 +0530

    added about page

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ console.log("Welcome");
 const hamburger = () => {
   var hamburgerDiv = document.querySelector("#hamburger");
   var navList = document.querySelector("#navList");
+  var backgroundContent = document.querySelector("#about-page-content");
 
-  console.log("clicked");
   hamburgerDiv.classList.toggle("is-active");
   navList.classList.toggle("show-list");
 };
